d7e446a53968e2c7aeca897ae4189beaedeb4fd8
Aka <ichia.aka@gmail.com>
2025-02-20 04:37:41 +0100

    Update license and text

--- Examples/CRTFilter.js
+++ Examples/CRTFilter.js
@@ -1,3 +1,32 @@
+/*!
+ * CRTFilterWebGL - A WebGL-based CRT effect for HTML5 Canvas
+ * 
+ * @version 1.0.0
+ * @license MIT
+ * @author Aka
+ * @repository https://github.com/yourusername/CRTFilterWebGL
+ * 
+ * Copyright (c) 2025 Aka
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 export class CRTFilterWebGL {
     constructor(canvas, config = {}) {
         if (!canvas) {
@@ -18,12 +47,12 @@ export class CRTFilterWebGL {
         }
 
         this.config = Object.assign({
-            barrelDistortion: 0.01,
-            chromaticAberration: 0.002,
-            staticNoise: 0.1,
-            horizontalTearing: 0.0012,
-            glowBloom: 0.01,
-            verticalJitter: 0.1,
+            barrelDistortion: 0.001,
+            chromaticAberration: 0.0002,
+            staticNoise: 0.001,
+            horizontalTearing: 0.00012,
+            glowBloom: 0.001,
+            verticalJitter: 0.001,
             retraceLines: true,
             dotMask: false,
             motionBlur: 0,
